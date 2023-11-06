02a70eba5df479c6d0225bf17f32645b6be41365
Marc McCarthy <marstheory20@gmail.com>
2023-11-06 15:10:55 -0500

    random cert in ecc

--- express-backend/src/index.js
+++ express-backend/src/index.js
@@ -4,12 +4,11 @@ const app = require("./app")
 const fs = require("fs");
 const https = require("https");
 const path = require("path");
-const httpsOptions = {
-    key: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/key.pem")),
-    cert: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/cert.pem"))
-};
-
 const envValidator = require("./config/config")
+const httpsOptions = {
+    key: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/localhost-ecc-key.pem")),
+    cert: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/localhost-ecc-cert.pem"))
+};
 
 logger.info("Checking configuration...")
 envValidator.validateServerConfiguration()
@@ -25,9 +24,13 @@ database.connect()
         const PORT = process.env.BACK_PORT
         const backUri = `https://${process.env.BACK_HOST}:${PORT}`
 
+        // https.createServer(httpsOptions, app).listen(PORT, () => {
+        //     logger.info(`Server running on ${backUri}`);
+        // });
+
         https.createServer(httpsOptions, app).listen(PORT, () => {
-            logger.info(`Server running on ${backUri}`)
-        });
+            logger.info(`Server running on ${backUri}`);
+          });
     
     }).catch((err) => {
         logger.error(err)
